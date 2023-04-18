fd0cf8ee5ec6dfa7ba87e2d050643de34aa0064a
zaki <zaki@localhost>
2023-04-18 15:58:32 -0700

    method 2nd

---
+++ src/test2/zaki1.java
@@ -0,0 +1,14 @@
+package test2;
+
+public class zaki1 {
+	public static void main(String[] args) {
+		System.out.println("This is a method where i first commit it to the normal repository and tahn");
+	}
+	
+	// method 2nd 
+	public static void age () {
+		System.out.println("age = 21");
+	}
+	
+	
+}
